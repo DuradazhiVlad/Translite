afb1bad849e2fddffcd57bc917bee5f27bed2d51
DuradazhiVlad <dvlad1995p@gmail.com>
2022-12-12 12:48:20 +0200

    Добавьте файлы проекта.

---
+++ Logic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translite
+{
+    public class Logic
+    {
+
+        public string a;
+        public string b;
+        public Logic(string a, string b)
+        {
+            this.a = a;
+
+            this.b = b;
+            if (a.StartsWith("Eng"))
+            {
+                    
+                foreach (var item in Ukr[b])
+                {
+                    Console.Write(item);
+                }
+            }
+            if (a.StartsWith("Ukr"))
+            {
+                    Console.WriteLine("Виберіть слово для перекладу|Привіт|,|Допобачення|");
+                foreach (var item in Eng[b])
+                {
+                    Console.Write(item);
+
+                }
+            }
+        }
+
+
+
+        public static Dictionary<string, string> Ukr = new Dictionary<string, string>()
+        {
+            ["Helow"] = "Привіт",
+            ["Goodbye"] = "Допобачення"
+
+        };
+
+        public static Dictionary<string, string> Eng = new Dictionary<string, string>()
+        {
+
+            ["Привіт"] = "Helow",
+            ["Допобачення"] = "Goodbye"
+        };
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Translite;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+Console.WriteLine("Виберіть мову на яку перекладати |Ukr| або |Eng|");
+string a = Console.ReadLine();
+
+if (a.StartsWith("Eng")) Console.WriteLine("Виберіть слово для перекладу|Helow|,|Goodbye|");
+if (a.StartsWith("Ukr")) Console.WriteLine("Виберіть слово для перекладу|Привіт|,|Допобачення|");
+    string b = Console.ReadLine();
+Logic logic = new Logic(a, b);
+
+
+
+
+
+
